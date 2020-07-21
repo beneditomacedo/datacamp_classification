d4d07e2e0f6935a19f9699af5da6ec5c2bf01d3f
Benedito Alberto Macedo <benedito.macedo@gmail.com>
2020-07-21 00:00:41 -0300

    Prepare dataset

---
+++ Chapter3/prepare_datasets.R
@@ -0,0 +1,12 @@
+## ---------------------------
+## Script name: prepare_datasets.R
+##
+## Purpose of script: Prepare the datasets for datacamp course Supervised Learning in R:
+## Classification, Chapter 3
+##
+## ---------------------------
+
+# load donors dataset
+donors <- read.csv("../data/donors.csv")
+# show where9am
+head(donors)
